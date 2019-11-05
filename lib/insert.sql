9e7e593137d0f99a53dc389ef5f43ae8aa65e849
Qing <chingwang.qw@gmail.com>
2019-11-05 22:43:29 +0000

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) 
+VALUES 
+('Mr.Chocolate', 3, 'female', 'black', 'nice', 
+true), 
+('Rowdy', 5, 'male', 'white', 'nice', true),
+('Melissa', 6, 'female', 'red', 'nice', true),
+('Tabitha', 7, 'male', 'brown', 'nice', true),
+('Wendy', 8, 'male', 'brown', 'angry', true),
+('Ginger', 9, 'male', 'grey', 'goofy', true),
+('Fido', 10, 'female', 'dark red', 'nice', true),
+( NULL, 11, 'male', 'black', 'goofy', true);
